d9b95cd611eee527dd089353fcaa2f5467c5b4ec
Madhu Lakshman <90201844+madhumadhu1318@users.noreply.github.com>
2023-08-20 20:55:21 +0530

    Create unsignedHighest.c

---
+++ DAY_1/unsignedHighest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	long long int max = (long long int ) (pow (2,10)* -1 );
+	printf("highest numner represented by long long int is %lld\n", max);
+	return 0;
+}
